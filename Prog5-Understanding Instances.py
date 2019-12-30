4f82557b54924b58f56067de529fb1d7a510ff86
aloks <srialok01@gmail.com>
2019-12-31 00:20:29 +0530

    Inital commit

--- Prog5-Understanding Instances.py
+++ Prog5-Understanding Instances.py
@@ -2,8 +2,11 @@
 Variables defined in class are available to the instances
 """
 print("====================Instance=====================")
+
+
 class MyClass(object):
     var = 10
+
 
 this_obj = MyClass()
 that_obj = MyClass()
@@ -16,13 +19,16 @@ print()
 """
 Accessing methods from instances
 """
+
+
 class salutation():
     def hello(self):
-        print("Hello there !!")
-        print(self)
+        print("Hello there !!")  # Hello there !!
+        print(self)  # <__main__.salutation object at 0x000001E7194B97C8>
+
 
 obj = salutation()
-print(obj)
+print(obj)  # <__main__.salutation object at 0x000001E7194B97C8>
 obj.hello()
 print()
 print("====================Instance Attributes===============")
@@ -31,12 +37,18 @@ Instance attributes
 """
 print()
 import random
+
+
 class Mynewclass():
     def dothis(self):
-        self.randaom_no = random.randint(1,10)
+        self.random_no = random.randint(1, 10)
+
 
 myinst = Mynewclass()
-print(myinst.dothis())
-print(myinst.randaom_no)
-
-
+# print(myinst.dothis()) # This will print None as the function doesn't have return type
+myinst.dothis() # Random no generation is done at the time of calling do this method
+print(myinst.random_no)
+"""
+myinst.random_no : this syntax is generally used to access a class variable, But there is no class variable here
+ as random value (i.e instance value) is set inside of an instance itself and hence its invoked by instance itself
+"""
